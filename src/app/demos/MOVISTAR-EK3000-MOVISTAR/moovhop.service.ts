5f5e25bc50665b2dadd6a7370156cde29b24e67d
ValentinGELLY <gelly.valentin@gmail.com>
2024-01-08 08:26:04 +0100

    init commit Movistar application

---
+++ src/app/demos/MOVISTAR-EK3000-MOVISTAR/moovhop.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AppService } from 'src/app/app.service';
+import { SoftKioskService } from 'src/app/softkiosk.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class MovistarService {
+
+
+
+}
